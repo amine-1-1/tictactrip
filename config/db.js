732d386260d1901ceb892863e1003c6454b078f5
amine-1-1 <57777108+amine-1-1@users.noreply.github.com>
2022-01-15 02:26:57 +0100

    commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useNewUrlParser: true 
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
