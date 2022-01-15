7209e61343a088f00465560f65bb4d2d32e9c596
amine-1-1 <57777108+amine-1-1@users.noreply.github.com>
2022-01-15 02:46:53 +0100

    commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+require("dotenv").config({
+    path: "./config/.env"
+});
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
         useUnifiedTopology: true,
-        useNewUrlParser: true 
+        useNewUrlParser: true
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
