d863722db424f32e9ca37eff9e2b149c36ea3c73
amine-1-1 <57777108+amine-1-1@users.noreply.github.com>
2022-01-15 10:13:24 +0100

    modified Swagger UI

--- routes/auth.js
+++ routes/auth.js
@@ -12,45 +12,83 @@ const router = express.Router();
 
 /**
  * @swagger
- *   /api/token:
- *     post:
- *       summary: Authenicate and recieve Token.
- *       tags: [Justify-Text]
- *       requestBody:
- *         required: true
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 email:
- *                   type: string
- *                   description: The user's email.
- *                   example: user2@email.com
- *       responses:
- *         200:
- *           description: Success
+ *   paths:
+ *     /api/token:
+ *       post:
+ *         summary: Authenicate and recieve Token.
+ *         tags: [Justify-Text]
+ *         requestBody:
+ *           required: true
  *           content:
  *             application/json:
  *               schema:
  *                 type: object
  *                 properties:
- *                   token:
+ *                   email:
  *                     type: string
- *                     example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIyQGVtYWlsLmNvbSIsImlhdCI6MTY0MjE3NjA1OCwiZXhwIjoxNjQyMTc5NjU4fQ.A_I95tvzc2QIDFmNVFi6wvJJT4sI03E4hvA7PORlfzs   
- *         400:
- *           description: Bad Request
+ *                     description: The user's email.
+ *                     example: user2@email.com
+ *         responses:
+ *           200:
+ *             description: Success
+ *             content:
+ *               application/json:
+ *                 schema:
+ *                   type: object
+ *                   properties:
+ *                     success:
+ *                       type: boolean
+ *                       example: true
+ *                     token:
+ *                       type: string
+ *                       example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIyQGVtYWlsLmNvbSIsImlhdCI6MTY0MjE3NjA1OCwiZXhwIjoxNjQyMTc5NjU4fQ.A_I95tvzc2QIDFmNVFi6wvJJT4sI03E4hvA7PORlfzs   
+ *           400:
+ *             description: Bad Request
+ *             content:
+ *               application/json:
+ *                 schema:
+ *                   type: object
+ *                   properties:
+ *                     sucess:
+ *                       type: string
+ *                       example: false
+ *                     error:
+ *                       type: string
+ *                       example: Invalid Credentials
+ * 
+ *     /api/register:
+ *       post:
+ *         summary: Register and recieve Token.
+ *         tags: [Justify-Text]
+ *         requestBody:
+ *           required: true
  *           content:
  *             application/json:
  *               schema:
  *                 type: object
  *                 properties:
- *                   sucess:
+ *                   name:
  *                     type: string
- *                     example: false
- *                   error:
+ *                     description: The user's name.
+ *                     example: amine
+ *                   email:
  *                     type: string
- *                     example: Invalid Credentials
+ *                     description: The user's email.
+ *                     example: amine@gmail.com
+ *         responses:
+ *           200:
+ *             description: Success
+ *             content:
+ *               application/json:
+ *                 schema:
+ *                   type: object
+ *                   properties:
+ *                     success:
+ *                       type: boolean
+ *                       example: true
+ *                     token:
+ *                       type: string
+ *                       example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIyQGVtYWlsLmNvbSIsImlhdCI6MTY0MjE3NjA1OCwiZXhwIjoxNjQyMTc5NjU4fQ.A_I95tvzc2QIDFmNVFi6wvJJT4sI03E4hvA7PORlfzs   
  */
 
 
