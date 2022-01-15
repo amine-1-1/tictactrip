fd096c2cf0eee7792767af673788b6231fc52491
amine-1-1 <57777108+amine-1-1@users.noreply.github.com>
2022-01-15 01:21:15 +0100

    third commit

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ const swaggerDefinition = {
     info: {
         title: 'API justify text',
         version: '1.0.0',
-        description: 'API Description Here',
+        description: 'this Api is done for the purpose of a technical test for tictactrip deliverables.\n * La longueur des lignes du texte justifié doit être de 80 caractères.\n * L’endpoint doit être de la forme /api/justify et doit retourner un texte justifié suite à une requête POST avec un body de ContentType text/plain \n * L’api doit utiliser un mécanisme d’authentification via token unique. En utilisant par exemple une endpoint api/token qui retourne un token d’une requête POST avec un json body {"email": "foo@bar.com"}.\n * Il doit y avoir un rate limit par token pour l’endpoint /api/justify, fixé à 80 000 mots par jour, si il y en a plus dans la journée il faut alors renvoyer une erreur 402 Payment Required.',
         contact: {
-            name: 'My Name',
-            email: 'My Email'
+            name: ' Mouhamed Amine Zaddem',
+            email: 'aminezaddem20@gmail.com'
         },
     },
     servers: [{
